d2cda015fad6808ee45478cb94626250132f9202
yoni-nachshon <601713@gmail.com>
2021-05-18 08:13:37 -0700

    Fix

--- src/app/components/register3/register3.component.ts
+++ src/app/components/register3/register3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from 'src/app/services/register.service';
 
 @Component({
   selector: 'app-register3',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register3.component.scss']
 })
 export class Register3Component implements OnInit {
+  name;
 
-  constructor() { }
-
+  constructor(private service:RegisterService) {
+    this.name = this.service.name;
+  }
+  
   ngOnInit(): void {
   }
   

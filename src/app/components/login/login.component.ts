969199a0893de1316f1b1f3087494327d8aa7d50
yoni-nachshon <601713@gmail.com>
2021-07-06 08:38:34 -0700

    login style

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,9 +9,10 @@ import { RegisterService } from 'src/app/services/register.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
   name;
   phone;
-  wrong = false;
+  
 
   constructor(private service:RegisterService,private router:Router) {
    }
@@ -24,13 +25,7 @@ export class LoginComponent implements OnInit {
     this.service.sendSms(true).subscribe(()=>{
       this.router.navigate(["/register2"]);
     });
-    // if(this.name == this.service.user.name && this.phone == this.service.user.phone){
-      
-    //   this.router.navigate(["/register2"]);
-    // }
-    // else {
-    //   this.wrong = true;
-    // }
+    
   }
 
 }
